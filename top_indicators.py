0677cf2778a6ec5d2b4402bacc2dc38ea5992f60
Anthony <pyskell@users.noreply.github.com>
2019-08-04 16:02:26 -0400

    ADD: Rename Result to Metric

--- top_indicators.py
+++ top_indicators.py
@@ -1,14 +1,19 @@
 from datetime import datetime, timedelta, date
+from enum import Enum, auto
 from pytrends.request import TrendReq
 from tabulate import tabulate
 from requests_html import HTMLSession
 from tabulate import tabulate
-from shared_vars import BITCOIN_PRICE, BITCOIN_AVERAGE_FEE, BITCOIN_MVRV
+from sqlalchemy import Column, Integer, UnicodeText, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
 import pandas as pd
 import numbers
 import requests
 import locale
 
+from shared_vars import BITCOIN_PRICE, BITCOIN_AVERAGE_FEE, BITCOIN_MVRV
 
 # All top indicators should reach 100% at their conservative estimate
 # We want to predict when we're very close to the top, 
@@ -23,14 +28,24 @@ import locale
 locale.setlocale(locale.LC_ALL, '')
 pytrends = TrendReq()
 
-class Result(object):
-	def __init__(self, name, description, current=0, remaining=0, target=0, units="days"):
+engine = create_engine('sqlite:///top_indicators.db')
+Base = declarative_base(bind=engine)
+
+class Indicator(Enum):
+	LEADING = auto() # Should reach its target *before* the top
+	LAGGING = auto() # Should reach its target *after* the top
+
+class Metric(object):
+	# __tablename__ = 'metrics'
+	# id = Column()
+	def __init__(self, name, description, current=0, remaining=0, target=0, units="days", indicator_type=Indicator.LEADING):
 		self.name = name
 		self.description = description
 		self.current = current
 		self.target = target
 		self.remaining = remaining
 		self.units = units
+		self.indicator_type = indicator_type
 
 	# TODO: In most cases remaining = target - current. May want to handle that in this object & create a special case for datetime/timedelta
 	@property
@@ -69,46 +84,52 @@ class Result(object):
 	
 
 def days_after_halvening():
-	result = Result("Top after halvening", "Price has historically reached a peak 400-500 days after halvening events")
+	metric = Metric("Top after halvening", "Price has historically reached a peak 400-500 days after halvening events", 
+		indicator_type=Indicator.LEADING)
 
 	halvening = datetime(2020, 5, 19) # Predicted halvening May 19 2020
 	end = halvening + timedelta(days=400) # 400-500 days after halvening
 
-	result.current = datetime.now()
-	result.target = end
-	result.remaining = (end - datetime.now()).days
+	metric.current = datetime.now()
+	metric.target = end
+	metric.remaining = (end - datetime.now()).days
 
-	return result
+	return metric
 
 
 def full_top_to_top_cycle():
-	result = Result("Full top-to-top cycle", "Cycle lengths appear to be getting longer, based on previous cycle of ~4 years")
+	metric = Metric("Full top-to-top cycle", "Cycle lengths appear to be getting longer, based on previous cycle of ~4 years", 
+		indicator_type=Indicator.LEADING)
 	# There's some evidence/belief that cycles are lengthening and in that case 6 -> 6.5 years is more appropriate
 
 	start = datetime(2017, 12, 17) # Top of last cycle
 	end = start + timedelta(days=1477)
 
-	result.current = datetime.now()
-	result.target = end
-	result.remaining = (end - datetime.now()).days
+	metric.current = datetime.now()
+	metric.target = end
+	metric.remaining = (end - datetime.now()).days
 
-	return result
+	return metric
 
 
 def price_from_previous_top():
-	result = Result("Price from previous top", "2011 -> 2013 was +3,500%, 2013 -> 2017 was +1,800%, prediction: +~700%", units="dollars")
+	metric = Metric("Price from previous top", "2011 -> 2013 was +3,500%, 2013 -> 2017 was +1,800%, prediction: +~700%", 
+		units="dollars", 
+		indicator_type=Indicator.LEADING)
 	# The above predicts a price of 19,000 * 9 = 171,000
 	# That is kind of nuts so a more "conservative" (but still nuts) target price of 135,000 was chosen
 
-	result.current = BITCOIN_PRICE
-	result.target = 15000 * 9
-	result.remaining = result.target - result.current
+	metric.current = BITCOIN_PRICE
+	metric.target = 15000 * 9
+	metric.remaining = metric.target - metric.current
 
-	return result
+	return metric
 
 
 def google_trends():
-	result = Result("Google trends top", "Google trends spike hard at tops. Any move >=80 index is probably top on the longest timeframe. > Caveat: This is an index", units="index")
+	metric = Metric("Google trends top", "Google trends spike hard at tops. Any move >=80 index is probably top on the longest timeframe. > Caveat: This is an index", 
+		units="index", 
+		indicator_type=Indicator.LEADING)
 	# Caveat: I'm not sure if indexes lag at all
 	# Monthly jumped to 39 in Nov 2017 prior to taking off to 100 in December
 
@@ -116,16 +137,17 @@ def google_trends():
 
 	trends = pytrends.interest_over_time()
 
-	result.current = trends.iloc[-1]['bitcoin'] # last row, bitcoin column
-	result.target = 80
-	result.remaining = result.target - result.current
+	metric.current = trends.iloc[-1]['bitcoin'] # last row, bitcoin column
+	metric.target = 80
+	metric.remaining = metric.target - metric.current
 
-	return result
+	return metric
 
 
 # TODO: Finish once we get API access
 def sopr():
-	result = Result("SOPR Ratio", "Tends to go >=1.04 near tops (also local tops)", units="index")
+	metric = Metric("SOPR Ratio", "Tends to go >=1.04 near tops (also local tops)", 
+		units="index")
 
 	session = HTMLSession()
 	request = session.get('https://studio.glassnode.com/metrics?a=BTC&m=valuation.Sopr')
@@ -143,11 +165,12 @@ def sopr():
 	sopr_int = request.html.find('.ant-statistic-content-value-int')
 	sopr_dec = request.html.find('.ant-statistic-content-value-decimal')
 
-	result.current = float(sopr_int.text + sopr_dec.text)
-	result.target = 1.04
-	result.remaining = result.target - result.current
+	metric.current = float(sopr_int.text + sopr_dec.text)
+	metric.target = 1.04
+	metric.remaining = metric.target - metric.current
 
-	return result
+	return metric
+
 
 
 def average_fee():
@@ -159,26 +182,30 @@ def average_fee():
 
 	# TODO: Median fees are also interesting.
 	# TODO: There seems to be a big run up in fees, a drop, and then another run up before each bubble popped
-	result = Result("Average Fee", "Last run when fees jumped >25 we were close to the top", units="dollars")
+	metric = Metric("Average Fee", "Last run when fees jumped >25 we were close to the top", 
+		units="dollars", 
+		indicator_type=Indicator.LEADING)
 
-	result.current = BITCOIN_AVERAGE_FEE
-	result.target = 25
-	result.remaining = result.target - result.current
+	metric.current = BITCOIN_AVERAGE_FEE
+	metric.target = 25
+	metric.remaining = metric.target - metric.current
 
-	return result
+	return metric
 
 
 def mvrv():
 	# MVRV differs slightly between CoinMetrics and Woobull's charts. The below uses CoinMetrics data.
 	# https://coinmetrics.io/charts/#assets=btc_right=CapMVRVCur
 	# https://charts.woobull.com/bitcoin-mvrv-ratio/
-	result = Result("MVRV", "Last runs showed peak MVRVs of ~7.5 (2011), ~5.6 (2013-start), ~5.8 (2013-end), ~4.6 (2017). Assuming ~4 for next top.", units="index")
+	metric = Metric("MVRV", "Last runs showed peak MVRVs of ~7.5 (2011), ~5.6 (2013-start), ~5.8 (2013-end), ~4.6 (2017). Assuming ~4 for next top.", 
+		units="index", 
+		indicator_type=Indicator.LEADING)
 
-	result.current = BITCOIN_MVRV
-	result.target = 4
-	result.remaining = result.target - result.current
+	metric.current = BITCOIN_MVRV
+	metric.target = 4
+	metric.remaining = metric.target - metric.current
 
-	return result
+	return metric
 
 if __name__ == "__main__":
 	indicators = [days_after_halvening(), full_top_to_top_cycle(), price_from_previous_top(), google_trends(), average_fee(), mvrv()]
