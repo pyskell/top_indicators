c5336dcfa8381187a706e40c4f52a2373d129f6c
Anthony <pyskell@users.noreply.github.com>
2019-11-17 09:38:22 -0500

    Finished refactor of top_indicators.py

--- top_indicators.py
+++ top_indicators.py
@@ -40,6 +40,10 @@ else:
 	engine = create_engine('sqlite:///top_indicators.db')
 Base = declarative_base(bind=engine)
 
+Base.metadata.create_all()
+Session = sessionmaker(bind=engine)
+s = Session()
+
 class Indicator(enum.Enum):
 	LEADING = auto() # Should reach its target *before* the top
 	LAGGING = auto() # Should reach its target *after* the top
@@ -115,13 +119,6 @@ class Metric(Base):
 		else:
 			return None
 
-Base.metadata.create_all()
-Session = sessionmaker(bind=engine)
-s = Session()
-
-
-# TODO: Make all these functions subclasses of Metric
-
 
 class DaysAfterHalvening(Metric):
 	def __init__(self, *args, **kwargs):
@@ -132,8 +129,6 @@ class DaysAfterHalvening(Metric):
 		self.current = datetime.now()
 		self.target = end
 		self.remaining = (end - datetime.now()).days
-
-days_after_halvening = DaysAfterHalvening("Top after halvening", "Price has historically reached a peak 400-500 days after halvening events", indicator_type=Indicator.LEADING)
 
 
 class FullTopToTopCycle(Metric):
@@ -147,9 +142,6 @@ class FullTopToTopCycle(Metric):
 		self.target = end
 		self.remaining = (end - datetime.now()).days
 
-# There's some evidence/belief that cycles are lengthening and in that case 6 -> 6.5 years is more appropriate
-full_top_to_top_cycle = FullTopToTopCycle("Full top-to-top cycle", "Cycle lengths appear to be getting longer, based on previous cycle of ~4 years",
-	indicator_type=Indicator.LEADING)
 
 class PriceFromPreviousTop(Metric):
 	def __init__(self, *args, **kwargs):
@@ -158,11 +150,6 @@ class PriceFromPreviousTop(Metric):
 		self.target = 15000 * 9
 		self.remaining = self.target - self.current
 
-# The below notes predict a price of 19,000 * 9 = 171,000
-# That is kind of nuts so a more "conservative" (but still nuts) target price of 135,000 was chosen
-price_from_previous_top = PriceFromPreviousTop("Price from previous top", "2011 -> 2013 was +3,500%, 2013 -> 2017 was +1,800%, prediction: +~700%",
-		units="dollars",
-		indicator_type=Indicator.LEADING)
 
 class GoogleTrends(Metric):
 	def __init__(self, *args, **kwargs):
@@ -174,12 +161,6 @@ class GoogleTrends(Metric):
 		self.current = int(trends.iloc[-1]['bitcoin']) # last row, bitcoin column
 		self.target = 80
 		self.remaining = int(self.target - self.current)
-
-# Caveat: I'm not sure if indexes lag at all
-# Monthly jumped to 39 in Nov 2017 prior to taking off to 100 in December
-google_trends = GoogleTrends("Google trends top", "Google trends spike hard at tops. Any move >=80 index is probably top on the longest timeframe. > Caveat: This is an index",
-		units="index",
-		indicator_type=Indicator.LEADING)
 
 
 # TODO: Finish once we get API access
@@ -221,10 +202,6 @@ class AverageFee(Metric):
 		self.target = 25
 		self.remaining = self.target - self.current
 
-average_fee = AverageFee("Average Fee", "Last run when fees jumped >25 we were close to the top",
-		units="dollars",
-		indicator_type=Indicator.LEADING)
-
 
 class MVRV(Metric):
 	def __init__(self, *args, **kwargs):
@@ -235,10 +212,6 @@ class MVRV(Metric):
 		self.current = BITCOIN_MVRV
 		self.target = 4
 		self.remaining = self.target - self.current
-
-mvrv = MVRV("MVRV", "Last runs showed peak MVRVs of ~7.5 (2011), ~5.6 (2013-start), ~5.8 (2013-end), ~4.6 (2017). Assuming ~4 for next top.",
-		units="index",
-		indicator_type=Indicator.LEADING)
 
 
 class GBTC(Metric):
@@ -256,8 +229,6 @@ class GBTC(Metric):
 		self.target = gbtc_market_pp_share * 1.8
 		self.remaining = self.target - self.current
 
-gbtc = GBTC("GBTC over NAV", "Last run GBTC market price traded at a peak of 2x NAV at the top. Looking for 1.8x. VanEck ETF may reduce NAV differential",
-		units="dollars")
 
 class FearAndGreed(Metric):
 	def __init__(self, *args, **kwargs):
@@ -267,9 +238,6 @@ class FearAndGreed(Metric):
 		self.target = 80
 		self.remaining = self.target - self.current
 
-fear_and_greed = FearAndGreed("Fear & Greed Index", "Good short-term indicator on whether or not the market is greedy. >80 is very concerning. >90 is gtfo.",
-		units="index")
-
 
 class TopCap(Metric):
 	def __init__(self, *args, **kwargs):
@@ -278,9 +246,6 @@ class TopCap(Metric):
 		self.current = BITCOIN_CURRENT_MCAP
 		self.target = BITCOIN_AVERAGE_MCAP * 32
 		self.remaining = self.target - self.current
-
-# *32 to play it safe and partially compensate for CoinMetrics data not matching Willy Woo's exactly (his goes back to 2010)
-top_cap = TopCap("Top Cap", "Bitcoin appears to hit tops when it's at 35x its forever average market cap. Targeting x32", units="dollars")
 
 
 class MayerMultiple(Metric):
@@ -292,16 +257,47 @@ class MayerMultiple(Metric):
 		self.target = 3.1
 		self.remaining = self.target - self.current
 
-mayer_multiple = MayerMultiple("Mayer Multiple", "Last bull run was ~3.8 at peak, this is declining though so targeting 3.1 (which is just a guess based on nothing)", units="ratio")
-
 
 if __name__ == "__main__":
-	# NOTE: Tabulate has an issue where if it encounters a value it can't format in a column it won't format the rest of the column
+	days_after_halvening = DaysAfterHalvening("Top after halvening", "Price has historically reached a peak 400-500 days after halvening events", 
+		indicator_type=Indicator.LEADING)
+	# There's some evidence/belief that cycles are lengthening and in that case 6 -> 6.5 years is more appropriate
+	full_top_to_top_cycle = FullTopToTopCycle("Full top-to-top cycle", "Cycle lengths appear to be getting longer, based on previous cycle of ~4 years",
+		indicator_type=Indicator.LEADING)
+	# The below notes predict a price of 19,000 * 9 = 171,000
+	# That is kind of nuts so a more "conservative" (but still nuts) target price of 135,000 was chosen
+	price_from_previous_top = PriceFromPreviousTop("Price from previous top", "2011 -> 2013 was +3,500%, 2013 -> 2017 was +1,800%, prediction: +~700%",
+		units="dollars",
+		indicator_type=Indicator.LEADING)
+	# Monthly jumped to 39 in Nov 2017 prior to taking off to 100 in December
+	google_trends = GoogleTrends("Google trends top", "Google trends spike hard at tops. Any move >=80 index is probably top on the longest timeframe. > Caveat: This is an index",
+		units="index",
+		indicator_type=Indicator.LEADING)
+	average_fee = AverageFee("Average Fee", "Last run when fees jumped >25 we were close to the top",
+		units="dollars",
+		indicator_type=Indicator.LEADING)
+	mvrv = MVRV("MVRV", "Last runs showed peak MVRVs of ~7.5 (2011), ~5.6 (2013-start), ~5.8 (2013-end), ~4.6 (2017). Assuming ~4 for next top.",
+		units="index",
+		indicator_type=Indicator.LEADING)
+	gbtc = GBTC("GBTC over NAV", "Last run GBTC market price traded at a peak of 2x NAV at the top. Looking for 1.8x. ETFs and other products may reduce differential",
+		units="dollars")
+	fear_and_greed = FearAndGreed("Fear & Greed Index", 
+		"Good short-term indicator on whether or not the market is greedy. >80 is very concerning. >90 is gtfo.",
+		units="index")
+	top_cap = TopCap("Top Cap", 
+		"Bitcoin appears to hit tops when it's at 35x its forever average market cap. Targeting x32", 
+		units="dollars")
+	mayer_multiple = MayerMultiple("Mayer Multiple", 
+		"Last bull run was ~3.8 at peak, this is declining though so targeting 3.1 (which is just a guess based on nothing)", 
+		units="ratio")
+	
 	metrics = [days_after_halvening, full_top_to_top_cycle, price_from_previous_top, average_fee, top_cap, gbtc, mayer_multiple, mvrv, google_trends, fear_and_greed]
 	results = []
 
 	for metric in metrics:
 
+		# NOTE: Tabulate has an issue where if it encounters a value it can't 
+		# format in a column it won't format the rest of the column
 		# TODO: This is ugly, make better
 		if not isinstance(metric.current, str):
 			results.append([metric.name, "{:,.2f}".format(metric.current), "{:,.2f}".format(metric.target), "{:,.2f}".format(metric.remaining), metric.units, f'{metric.description}\n{metric.progress_bar if metric.progress_bar else ""}'])
